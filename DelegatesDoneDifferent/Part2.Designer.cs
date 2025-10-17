acd76dc88d6d75d572e5c21a82228c68550b1ae2
Steven Contos <steve.contos@gmail.com>
2025-10-17 14:32:56 -0500

    Add project files.

---
+++ DelegatesDoneDifferent/Part2.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace DelegatesDoneDifferent
+{
+    partial class Part2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExample1 = new System.Windows.Forms.Button();
+            this.btnExample2 = new System.Windows.Forms.Button();
+            this.btnExample3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(205, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 162);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "This form supports the excersises shown in Part Two of Delegates Done Differently" +
+    "";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnExample1
+            // 
+            this.btnExample1.Location = new System.Drawing.Point(30, 36);
+            this.btnExample1.Name = "btnExample1";
+            this.btnExample1.Size = new System.Drawing.Size(106, 23);
+            this.btnExample1.TabIndex = 8;
+            this.btnExample1.Text = "Example 2.1";
+            this.btnExample1.UseVisualStyleBackColor = true;
+            this.btnExample1.Click += new System.EventHandler(this.btnExample1_Click);
+            // 
+            // btnExample2
+            // 
+            this.btnExample2.Location = new System.Drawing.Point(30, 65);
+            this.btnExample2.Name = "btnExample2";
+            this.btnExample2.Size = new System.Drawing.Size(106, 23);
+            this.btnExample2.TabIndex = 9;
+            this.btnExample2.Text = "Example 2.2";
+            this.btnExample2.UseVisualStyleBackColor = true;
+            this.btnExample2.Click += new System.EventHandler(this.btnExample2_Click);
+            // 
+            // btnExample3
+            // 
+            this.btnExample3.Location = new System.Drawing.Point(30, 94);
+            this.btnExample3.Name = "btnExample3";
+            this.btnExample3.Size = new System.Drawing.Size(106, 23);
+            this.btnExample3.TabIndex = 10;
+            this.btnExample3.Text = "Example 2.3";
+            this.btnExample3.UseVisualStyleBackColor = true;
+            this.btnExample3.Click += new System.EventHandler(this.btnExample3_Click);
+            // 
+            // Part2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 253);
+            this.Controls.Add(this.btnExample3);
+            this.Controls.Add(this.btnExample2);
+            this.Controls.Add(this.btnExample1);
+            this.Controls.Add(this.label1);
+            this.Name = "Part2";
+            this.Text = "Simple Test Form - Part 2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExample1;
+        private System.Windows.Forms.Button btnExample2;
+        private System.Windows.Forms.Button btnExample3;
+    }
+}
---
+++ DelegatesDoneDifferent/Part2.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DelegatesDoneDifferent
+{
+    public partial class Part2 : Form
+    {
+        public Part2()
+        {
+            InitializeComponent();
+        }
+
+        //Each button is wrapped in it's own region which refers the buttons actions and supporting code. 
+
+        #region Example One
+
+        private void btnExample1_Click(object sender, EventArgs e)
+        {
+            //We are going to be testin integer values
+            DelegateValidator<int> dint = new DelegateValidator<int>();
+
+            bool retval;
+
+            //Create a linked list of clsValidator, Current validator test to see if Value is greater than 1, 
+            //if so then next validator (PassValidator) tests to see if Value is also < 10
+            dint.ValidationRules = new clsValidator<int> { 
+                CurrentValidator = (i) => { return i > 0; }, 
+                PassValidator = new clsValidator<int> { 
+                    CurrentValidator = (i) => { return i < 10; } 
+                } 
+            };
+
+            dint.Value = -1;
+            retval = dint.Valid(); //False, less then 0 - fails first validator, never goes to second validator
+
+            dint.Value = 5;
+            retval = dint.Valid(); //True, greater then 0 and less then 10 - both validators return true
+
+            dint.Value = 50;
+            retval = dint.Valid(); //False, greater then 0 and greater then 10 - first validator passes, second fails.
+
+
+        }
+
+        #endregion
+
+        #region Example Two
+        private void btnExample2_Click(object sender, EventArgs e)
+        {
+            //First test - defining validator with anonymous functions
+            DelegateValidator<string> delstr = new DelegateValidator<string>();
+            delstr.Value = "Steve";
+            //inline validation - first length > 1 then first char is Upper case
+            delstr.ValidationRules = new clsValidator<string>() 
+                { CurrentValidator = (s) => { return s.Trim().Length > 1; }, 
+                    PassValidator = new clsValidator<string>() 
+                    { CurrentValidator = (s) => { return char.IsUpper(s, 0); } } 
+                };
+            bool isValid = delstr.Valid(); // With "Steve", Validation passes.
+
+            //Second Test - defining validators with functions which match the signature.
+            clsValidator<string> mv = new clsValidator<string>();
+            mv.CurrentValidator = LengthMinValidator; //Check to see if it passes min length
+            mv.PassValidator = new clsValidator<string>(); //If so,
+            mv.PassValidator.CurrentValidator = LengthMaxValidator; //Check to see if max length
+
+            mv.PassValidator.PassValidator = new clsValidator<string>() { 
+                CurrentValidator = FirstLetterUpperValidator };
+            
+            mv.PassValidator.FailValidator = new clsValidator<string>();
+            //If max validator fails, check to see if number.
+            mv.PassValidator.FailValidator.CurrentValidator = IsNumberValidator; 
+
+
+            delstr.ValidationRules = mv; //Assigning new rules.
+            delstr.Value = "cat";
+            isValid = delstr.Valid(); //Should be false, passes min and max but not Upper case;
+
+            delstr.Value = "10000";
+            isValid = delstr.Valid(); //Should be true, long and numeric
+
+            delstr.Value = "10";
+            isValid = delstr.Valid(); //Should be false, not long and not upper case;
+
+            delstr.Value = "Cat";
+            isValid = delstr.Valid(); //Should be true;
+
+        }
+        private bool LengthMinValidator(string s)
+        {
+            Console.WriteLine("LengthMinValidator");
+            return s.Length > 1;
+        }
+
+        private bool LengthMaxValidator(string s)
+        {
+            Console.WriteLine("LengthMaxValidator");
+            return s.Length < 5;
+        }
+
+        private bool FirstLetterUpperValidator(string s)
+        {
+            Console.WriteLine("FirstLetterUpperValidator");
+            return Char.IsUpper(s, 0);
+        }
+
+        private bool IsNumberValidator(string s)
+        {
+            Console.WriteLine("IsNumberValidator");
+            long j;
+            return Int64.TryParse(s, out j);
+        }
+
+        #endregion
+
+        #region Example Three
+        private void btnExample3_Click(object sender, EventArgs e)
+        {
+            clsEmployee ce1 = new clsEmployee() { 
+                FirstName = "Bob", LastName = "Marley" 
+            };
+            clsEmployee ce2 = new clsEmployee() { 
+                FirstName = "Steve", 
+                LastName = "Contos",
+                Webpage = @"http://www.PolarisSolutions.com" 
+            };
+            //will fail on both webpage and if you fix page will fail on age
+            clsEmployee ce3 = new clsEmployee() { 
+                FirstName = "Bill", 
+                LastName = "Gates", 
+                Webpage = @"http://wwwMicrosoftcom",
+                Age = 10, 
+                BirthDate = Convert.ToDateTime("10/28/1955").Date 
+            };
+
+            clsEmployee ce4 = new clsEmployee()
+            {
+                FirstName = "Steve"
+            };
+
+            /*Test
+                1. First Name has value
+                2. Second Name has value
+                3. If WebPage supplied, confirm valid
+                4. If Age > 0 validate BirthDate
+            */
+
+            DelegateValidator<clsEmployee> dv = new DelegateValidator<clsEmployee>();
+            dv.ValidationRules = new clsValidator<clsEmployee>()
+            {
+                CurrentValidator = checkFirstName,
+                PassValidator = new clsValidator<clsEmployee>()
+                {
+                    //First name passes
+                    CurrentValidator = checkLastName,
+                    PassValidator = new clsValidator<clsEmployee>()
+                    {
+                        //Last name passes
+                        CurrentValidator = checkWebAddress,
+                        PassValidator = new clsValidator<clsEmployee>()
+                        {
+                            //Web address passes
+                            CurrentValidator = checkAge
+                        }
+                    }
+                }
+            };
+
+            bool retval = false;
+
+            dv.Value = ce1;
+            retval = dv.Valid(); //Should pass
+
+            dv.Value = ce2;
+            retval = dv.Valid(); //Should pass
+
+            dv.Value = ce3;
+            retval = dv.Valid(); //Should fail
+
+            dv.Value = ce4;
+            retval = dv.Valid(); //Should fail
+
+
+        }
+
+        private bool checkFirstName(clsEmployee e)
+        {
+            return e.FirstName != null && e.FirstName.Length > 0;
+        }
+        private bool checkLastName(clsEmployee e)
+        {
+            return e.LastName != null && e.LastName.Length > 0;
+        }
+
+        private bool checkWebAddress(clsEmployee e)
+        {
+            //Regex from google search :)    
+            Regex rg = 
+                new Regex(@"[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)");
+            if (string.IsNullOrEmpty(e.Webpage))
+                return true;
+            if (e.Webpage.Trim().Length == 0)
+                return true; // not an error
+            else
+                return rg.IsMatch(e.Webpage);
+        }
+
+        private bool checkAge(clsEmployee e)
+        {
+            if (e.Age > 0)
+            {
+                if (DateTime.Now.Year - e.BirthDate.Year == e.Age)
+                    return true;
+
+                return false;
+            }
+            else
+                return true; //not an error not to enter an age
+        
+        }
+
+
+
+        #endregion
+
+    }
+    #region Example Helper Classes
+    class clsValidator<T>
+    {
+        public Func<T, bool> CurrentValidator { get; set; }
+        public clsValidator<T> FailValidator { get; set; }
+        public clsValidator<T> PassValidator { get; set; }
+
+    }
+
+    class DelegateValidator<T>
+    {
+
+        public T Value { get; set; }
+
+        public clsValidator<T> ValidationRules { get; set; }
+
+        internal bool Valid()
+        {
+            //Seed the recursive routine with root node
+            return Valid(ValidationRules, Value);
+        }
+        /// <summary>
+        /// This is a recursive call which traverses the clsValidator nodes.
+        /// </summary>
+        /// <param name="currval">Validator function</param>
+        /// <param name="RecurseValue">Value</param>
+        /// <returns>true or false</returns>
+        private bool Valid(clsValidator<T> currval, T RecurseValue)
+        {
+
+            //invoke the Func<T,bool> to test the Value
+            bool success = currval.CurrentValidator.Invoke(RecurseValue);
+            if (success)
+            {
+                if (currval.PassValidator != null)
+                    return Valid(currval.PassValidator, RecurseValue);
+            }
+            else
+            {
+                if (currval.FailValidator != null)
+                    return Valid(currval.FailValidator, RecurseValue);
+            }
+
+
+            return success;
+
+
+        }
+
+    }
+
+    class clsEmployee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Webpage { get; set; }
+
+    
+    }
+
+    #endregion
+}
